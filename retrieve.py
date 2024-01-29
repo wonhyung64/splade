fd4ca9f8186d0837ca57de0a293bac868ff6bbbd
Wonhyung Shin <wonhyung64@icloud.com>
2024-01-29 12:28:08 +0000

    feat - copy

---
+++ retrieve.py
@@ -0,0 +1,41 @@
+#%%
+# import hydra
+from omegaconf import DictConfig
+import os
+
+from conf.CONFIG_CHOICE import CONFIG_NAME, CONFIG_PATH
+from splade.datasets.dataloaders import CollectionDataLoader
+from splade.datasets.datasets import CollectionDatasetPreLoad
+from splade.evaluate import evaluate
+from splade.models.models_utils import get_model
+from splade.tasks.transformer_evaluator import SparseRetrieval
+from splade.utils.utils import get_dataset_name, get_initialize_config
+
+
+@hydra.main(config_path=CONFIG_PATH, config_name=CONFIG_NAME, version_base="1.2")
+def retrieve_evaluate(exp_dict: DictConfig):
+    exp_dict, config, init_dict, model_training_config = get_initialize_config(exp_dict)
+
+    #if HF: need to udate config.
+    if "hf_training" in config:
+       init_dict.model_type_or_dir=os.path.join(config.checkpoint_dir,"model")
+       init_dict.model_type_or_dir_q=os.path.join(config.checkpoint_dir,"model/query") if init_dict.model_type_or_dir_q else None
+
+
+    model = get_model(config, init_dict)
+
+    batch_size = 1
+    # NOTE: batch_size is set to 1, currently no batched implem for retrieval (TODO)
+    for data_dir in set(exp_dict["data"]["Q_COLLECTION_PATH"]):
+        q_collection = CollectionDatasetPreLoad(data_dir=data_dir, id_style="row_id")
+        q_loader = CollectionDataLoader(dataset=q_collection, tokenizer_type=model_training_config["tokenizer_type"],
+                                        max_length=model_training_config["max_length"], batch_size=batch_size,
+                                        shuffle=False, num_workers=1)
+        evaluator = SparseRetrieval(config=config, model=model, dataset_name=get_dataset_name(data_dir),
+                                    compute_stats=True, dim_voc=model.output_dim)
+        evaluator.retrieve(q_loader, top_k=exp_dict["config"]["top_k"], threshold=exp_dict["config"]["threshold"])
+    evaluate(exp_dict)
+
+
+if __name__ == "__main__":
+    retrieve_evaluate()
